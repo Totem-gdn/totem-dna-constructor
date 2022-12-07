98bc42575b4ac16eba09e1783b2ce46b3aeec206
Roman Kostiv <roman120487@ukr.net>
2022-12-07 16:52:14 +0200

    make placeholder instead empty form

--- src/app/modules/main/components/property-form/property-form.component.ts
+++ src/app/modules/main/components/property-form/property-form.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { PropertyModel } from '../../models/property.model';
 
 @Component({
   selector: 'app-property-form',
   templateUrl: './property-form.component.html',
-  styleUrls: ['./property-form.component.scss']
+  styleUrls: ['./property-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PropertyFormComponent implements OnInit {
-
+  @Input() property?: PropertyModel;
   constructor() { }
 
   ngOnInit(): void {
