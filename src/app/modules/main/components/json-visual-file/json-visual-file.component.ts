7e4b11dc6f64688592fd578eeaad0311b3aed6d1
Roman Kostiv <roman120487@ukr.net>
2022-12-13 20:20:02 +0200

    make download json file

--- src/app/modules/main/components/json-visual-file/json-visual-file.component.ts
+++ src/app/modules/main/components/json-visual-file/json-visual-file.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { PropertyModel } from '../../models/property.model';
 
 @Component({
@@ -6,13 +7,31 @@ import { PropertyModel } from '../../models/property.model';
   templateUrl: './json-visual-file.component.html',
   styleUrls: ['./json-visual-file.component.scss']
 })
-export class JsonVisualFileComponent implements OnInit {
+export class JsonVisualFileComponent implements OnInit, OnChanges {
   @Input() propertyList!: PropertyModel[];
-  constructor() { }
+  jsonAsString: string = '';
+  jsonUrl: any;;
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
     console.log('propertyList', this.propertyList);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('change');
     
+    this.jsonAsString = JSON.stringify(this.propertyList);
+    this.createJsonForDownload();
+  }
+
+  createJsonForDownload(): void {
+    const theJSON = JSON.stringify(this.propertyList);
+    const blob = new Blob([theJSON],{type: 'text/json'});
+    const url = window.URL.createObjectURL(blob);
+    const uri: SafeUrl = this.sanitizer.bypassSecurityTrustUrl(url);
+    this.jsonUrl = uri;
   }
 
 }
